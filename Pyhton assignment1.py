ae2c1e9c61c679b967391b8874f4426734f0a7e1
Hrushikesh Sonawale <111571918+Hrushi1432@users.noreply.github.com>
2024-10-25 01:22:21 +0530

    Add files via upload

---
+++ Pyhton assignment1.py
@@ -0,0 +1,16 @@
+#print("Hello Python")
+# num1=float(input("enter1:"))
+# num2=float(input("enter2:"))
+# print("Addition:",num1+num2)
+# print("Division:",num1/num2)
+# Base=float(input("Enter Base:"))
+# Height=float(input("Enter Height:"))
+# print("Area Of Triangle:",0.5*Base*Height)
+# x = 5
+# y = 10
+# x, y = y, x
+# print("x =", x)
+# print("y =", y)
+# import random
+# n=random.randint(1,100)
+# print(n)
